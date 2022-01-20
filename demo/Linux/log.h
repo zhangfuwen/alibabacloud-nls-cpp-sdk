65e784e8757ec3664084ef3736fc932485de8a2c
zhangfuwen <zhangfuwen@bytedance.com>
2022-01-20 15:19:04 +0800

    log with time

--- demo/Linux/log.h
+++ demo/Linux/log.h
@@ -11,6 +11,7 @@
 #include <cstdio>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 constexpr auto filename(std::string_view path)
 {
@@ -24,14 +25,17 @@ static_assert(filename("file.cpp") == "file.cpp");
 
 #define LOG_INFO(fmt, ...)                                                     \
     do {                                                                       \
+        char buff[40]; \
+        time_t now = time(NULL); \
+        strftime(buff, 40, "%Y-%m-%d %H:%M:%S.%f", localtime(&now)); \
         if(logfd)                                                              \
         {                                                                      \
-            fprintf(logfd, "%s:%d %s > " fmt "\n", filename(__FILE__).data(),          \
-                   __LINE__, __FUNCTION__, ##__VA_ARGS__);                     \
+            fprintf(logfd, "%s %s:%d %s > " fmt "\n", buff, filename(__FILE__).data(),          \
+                    __LINE__, __FUNCTION__, ##__VA_ARGS__);                     \
             fflush(logfd);                                                                       \
         }                                                                       \
-        printf("%s:%d %s > " fmt "\n", filename(__FILE__).data(),          \
-               __LINE__, __FUNCTION__, ##__VA_ARGS__);                     \
+        printf("%s %s:%d %s > " fmt "\n", buff, filename(__FILE__).data(),          \
+                __LINE__, __FUNCTION__, ##__VA_ARGS__);                     \
     } while (0)
 //if (logfd >= 0) {                                                      \
 //    dprintf(logfd, "%s:%d %s > " fmt "\n", filename(__FILE__).data(),  \
@@ -43,27 +47,30 @@ static_assert(filename("file.cpp") == "file.cpp");
 #define LOG_ERROR LOG_INFO
 
 inline FILE * logfd = nullptr;
-inline int dumpfd = -1;
+inline FILE * dumpfd = nullptr;
 
 inline void log_init() {
-    logfd = fopen("/home/zhangfuwen/audio_ime_log.txt", "w+");
-    dumpfd = open("/home/zhangfuwen/audio_ime_dump.txt", O_CREAT|O_RDWR | O_APPEND, 0644);
+    logfd = fopen("/home/zhangfuwen/audio_ime_log.txt", "a+");
+    dumpfd = fopen("/home/zhangfuwen/audio_ime_dump.txt", "a+");
 }
 
-inline void dump() {
-    void *array[40];
-    size_t size;
+static inline void printBacktrace() {
+    char buff[40];
+    time_t now = time(NULL);
+    strftime(buff, 40, "%Y-%m-%d %H:%M:%S.%f", localtime(&now));
 
-    if(dumpfd >= 0) {
-        // get void*'s for all entries on the stack
-        size = backtrace(array, 40);
-        // print out all the frames to stderr
-        backtrace_symbols_fd(array, size, dumpfd);
+    fprintf(dumpfd, "%s crash:\n", buff);
+    void *stackBuffer[64];
+    int numAddresses = backtrace((void**) &stackBuffer, 64);
+    char **addresses = backtrace_symbols(stackBuffer, numAddresses);
+    for( int i = 0 ; i < numAddresses ; ++i ) {
+        fprintf(dumpfd, "[%2d]: %s\n", i, addresses[i]);
     }
+    free(addresses);
 }
 
 inline void signal_handler(int signo) {
-    dump();
+    printBacktrace();
     signal(signo, SIG_DFL);
     raise(signo);
 }
